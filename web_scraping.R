165dac0bcf182b6d699356cd901f056012c2e990
DhrubaDR <dasroy.dhruba@gmail.com>
2021-09-08 12:01:24 +0530

    2nd_Commit

---
+++ web_scraping.R
@@ -0,0 +1,56 @@
+# import a webpage into R
+library(rvest)
+url <- "https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state"
+h <- read_html(url)
+class(h)
+h
+
+tab <- h %>% html_nodes("table")
+tab <- tab[[2]]
+
+tab <- tab %>% html_table
+class(tab)
+
+tab <- tab %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
+head(tab)
+
+######
+# read in raw murders data from Wikipedia
+url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
+murders_raw <- read_html(url) %>% 
+  html_nodes("table") %>% 
+  html_table() %>%
+  .[[1]] %>%
+  setNames(c("state", "population", "total", "murder_rate"))
+
+# inspect data and column classes
+head(murders_raw)
+class(murders_raw$population)
+class(murders_raw$total)
+
+
+##########################################################
+# murders_raw defined in web scraping video
+
+# direct conversion to numeric fails because of commas
+murders_raw$population[1:3]
+#as.numeric(murders_raw$population[1:3])
+
+library(tidyverse)    # includes stringr
+
+# murders_raw was defined in the web scraping section
+
+# detect whether there are commas
+commas <- function(x) any(str_detect(x, ","))
+murders_raw %>% summarize_all(funs(commas))
+
+# replace commas with the empty string and convert to numeric
+test_1 <- str_replace_all(murders_raw$population, ",", "")
+test_1 <- as.numeric(test_1)
+
+# parse_number also removes commas and converts to numeric
+test_2 <- parse_number(murders_raw$population)
+identical(test_1, test_2)
+
+murders_new <- murders_raw %>% mutate_at(2:3, parse_number)
+murders_new %>% head
